9da8ec65f0118d02e79999b22ccfe0566e4c5c29
Leograzioli <leograzioli@gmail.com>
2022-11-28 17:41:01 +0100

    add css

--- response.php
+++ response.php
@@ -10,16 +10,19 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <h2>
-        <?php
-        echo $_SESSION["pw-lenght"];
-        ?>
-    </h2>
-    
-    <a href="index.php">ritorna alla home</a>
+    <div class="wrapper">
+        <h2>your password is:
+            <?php
+            echo $_SESSION["pw-lenght"];
+            ?>
+        </h2>
+        <a href="index.php">return to home</a>
+    </div>
+
 </body>
 
 </html>
